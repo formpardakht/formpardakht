ef63ea6ccf1112067057ec147a50d3f7d3cc7b0d
Saeed Vaziry <sa.vaziry@gmail.com>
2019-09-07 21:43:41 +0430

    fix installation middleware

--- core/app/Http/Middleware/CheckInstallation.php
+++ core/app/Http/Middleware/CheckInstallation.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use DB;
 
 class CheckInstallation
 {
@@ -17,6 +18,16 @@ class CheckInstallation
      */
     public function handle($request, Closure $next)
     {
+        // check old database for version 2.6.0
+        try {
+            $siteUrl = DB::table('configs')->where('key', '=', 'site_url')->first();
+            if ($siteUrl) {
+                return $next($request);
+            }
+        } catch (\Exception $e) {
+            //
+        }
+
         try {
             if (site_config('site_url')) {
                 return $next($request);
